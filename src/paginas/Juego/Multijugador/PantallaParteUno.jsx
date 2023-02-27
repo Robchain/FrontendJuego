94deb2b6596d67234a4236898cfc5344167d124c
Robert Roman <ositorobertroman@hotmail.com>
2023-02-27 10:17:08 -0500

    lunes

--- src/paginas/Juego/Multijugador/PantallaParteUno.jsx
+++ src/paginas/Juego/Multijugador/PantallaParteUno.jsx
@@ -3,17 +3,22 @@ import { Container } from 'reactstrap'
 import { NavBarJuego } from '../../../componentes/JuegoComponent/JuegoGeneral/NavBarJuego'
 import { Espera } from '../../../componentes/MultiJugador/Espera'
 import { SeleccionDeEquipo } from '../../../componentes/MultiJugador/SeleccionDeEquipo'
+import { VerProgresoYaTerminado } from '../../../componentes/MultiJugador/VerProgresoYaTerminado'
 
 export const PantallaParteUno = () => {
   return (
     <Container>
          <NavBarJuego Seccion={"Activdad Asincrono"} urlBack={"/MenuJuego"}/>
-
+         
+          {/*
+         <Espera/>
+          
+         <VerProgresoYaTerminado/>
+         
+          */}
         
-         {/*
-          <Espera/>
-         */}
          <SeleccionDeEquipo/>
+          
     </Container>
   )
 }
