62a4e45634da31a3823ae26cd6650bcb91f1b6e0
Robert <ositorobertroman@hotmail.com>
2024-04-20 00:37:54 -0500

    sabado

--- src/paginas/Administrador/ActividadColaborativaAdm.js
+++ src/paginas/Administrador/ActividadColaborativaAdm.js
@@ -8,7 +8,7 @@ import { PasoUnoFormulario } from "../../componentes/Administrador/PasoUnoFormul
 import { PasoDosFormulario } from "../../componentes/Administrador/PasoDosFormulario";
 import { PasoTresFormulario } from "../../componentes/Administrador/PasoTresFormulario";
 import { PasoCuatroFormulario } from "../../componentes/Administrador/PasoCuatroFormulario";
-import {  ordenarYagrupar } from "../../helpers/contador";
+import { ordenarYagrupar } from "../../helpers/contador";
 import { LlamadaDeLLenadoDeEstudianteMultiJugador, historialJuego } from "../../service/Multijugador";
 import { ListadoHistoriaDeAsignaciones } from "../../componentes/Administrador/ListadoHistoriaDeAsignaciones";
 import { MostrarCurso, MostrarParalelo } from "../../service/Adminstrador/Usuarios";
@@ -60,12 +60,12 @@ const ActividadColaborativaAdm = () => {
       setEstudiantes([]);
     }
   }
-  const dataCurso = async ()=>{
+  const dataCurso = async () => {
 
     const data = await MostrarCurso();
     setcursoData(data);
   }
-  const dataParalelo = async ()=>{
+  const dataParalelo = async () => {
     const data = await MostrarParalelo();
     setparaleloData(data)
   }
@@ -171,67 +171,67 @@ const ActividadColaborativaAdm = () => {
           </Button>
         </Col>
         <Nav tabs style={{ fontSize: 14 }} >
-            <NavItem>
-              <NavLink
-                style={{ color: "#62259E" }}
-                onClick={() => { setTabs("1") }}
-              >
-                 Crear Asignación
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                style={{ color: "#62259E" }}
-                onClick={() => { setTabs("2") }}
-              >
-                Historial de juegos
-              </NavLink>
-            </NavItem>
-          </Nav>
+          <NavItem>
+            <NavLink
+              style={{ color: "#62259E" }}
+              onClick={() => { setTabs("1") }}
+            >
+              Crear Asignación
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              style={{ color: "#62259E" }}
+              onClick={() => { setTabs("2") }}
+            >
+              Historial de juegos
+            </NavLink>
+          </NavItem>
+        </Nav>
       </Row>
       <TabContent activeTab={tabs} className="tabvs">
-      {
-        Estudiantes.length >0 && <>
-          {
-            Estudiantes[0].value !=="NO HAY ESTUDIANTES" ?    <TabPane tabId="1" >
+        {
+          Estudiantes.length > 0 && <>
+            {
+              Estudiantes[0].value !== "NO HAY ESTUDIANTES" ? <TabPane tabId="1" >
 
-            <Row className="mt-4">
-              <Stepbar steps={index} />
-              {
-                index === 1 && <PasoUnoFormulario estudiantevalue={Estudiantes.length} TipoDeJuego={TipoDeJuego} index={index} nextButton={nextButton} prevButton={prevButton} dispatch={dispatch} NumeroDeGrupos={NumeroDeGrupos} NumeroDeIntegrantes={NumeroDeIntegrantes} NombreDeEquipo={NombreDeEquipo}  />
-              }
-              {
-                index === 2 && <PasoDosFormulario dispatch={dispatch} AleotorioArmado={AleotorioArmado} onClickAleatorio={onClickAleatorio} Aleatorio={Aleatorio} Estudiantes={Estudiantes} index={index} nextButton={nextButton} prevButton={prevButton} setSegundo={setSegundo} NumeroDeGrupos={NumeroDeGrupos} NumeroDeIntegrantes={NumeroDeIntegrantes} />
-              }
-              {
-                index === 3 && <PasoTresFormulario index={index} nextButton={nextButton} prevButton={prevButton} setPicker={setPicker} picker={picker} picker2={picker2} setPicker2={setPicker2}/>
-              }
-              {
-                index === 4 && <PasoCuatroFormulario Curso={Curso} Paralelo={Paralelo} index={index} nextButton={nextButton} prevButton={prevButton} Segundo={Segundo} picker2={picker2} NombreDeEquipo={NombreDeEquipo} NumeroDeGrupos={NumeroDeGrupos} NumeroDeIntegrantes={NumeroDeIntegrantes} picker={picker} TipoDeJuego={TipoDeJuego} />
-              }
-            </Row> </TabPane>:<>
-            NO HAY SUFICIENTES ESTUDIANTES
-            </>
-          }
-                  <TabPane tabId="2" >{
-            historial.length > 0 && <div> <br /> <h4 style={{ color: "#85858C", fontSize: '1.5em' }}>Historial de juegos</h4> 
-            {historial.map(i=>(
- <ListadoHistoriaDeAsignaciones data={i.documentos}/>
-            ))
+                <Row className="mt-4">
+                  <Stepbar steps={index} />
+                  {
+                    index === 1 && <PasoUnoFormulario estudiantevalue={Estudiantes.length} TipoDeJuego={TipoDeJuego} index={index} nextButton={nextButton} prevButton={prevButton} dispatch={dispatch} NumeroDeGrupos={NumeroDeGrupos} NumeroDeIntegrantes={NumeroDeIntegrantes} NombreDeEquipo={NombreDeEquipo} />
+                  }
+                  {
+                    index === 2 && <PasoDosFormulario dispatch={dispatch} AleotorioArmado={AleotorioArmado} onClickAleatorio={onClickAleatorio} Aleatorio={Aleatorio} Estudiantes={Estudiantes} index={index} nextButton={nextButton} prevButton={prevButton} setSegundo={setSegundo} NumeroDeGrupos={NumeroDeGrupos} NumeroDeIntegrantes={NumeroDeIntegrantes} />
+                  }
+                  {
+                    index === 3 && <PasoTresFormulario index={index} nextButton={nextButton} prevButton={prevButton} setPicker={setPicker} picker={picker} picker2={picker2} setPicker2={setPicker2} />
+                  }
+                  {
+                    index === 4 && <PasoCuatroFormulario Curso={Curso} Paralelo={Paralelo} index={index} nextButton={nextButton} prevButton={prevButton} Segundo={Segundo} picker2={picker2} NombreDeEquipo={NombreDeEquipo} NumeroDeGrupos={NumeroDeGrupos} NumeroDeIntegrantes={NumeroDeIntegrantes} picker={picker} TipoDeJuego={TipoDeJuego} />
+                  }
+                </Row> </TabPane> : <>
+                NO HAY SUFICIENTES ESTUDIANTES
+              </>
             }
-           </div>
-          }
-</TabPane>
-        </>
-      }
-      
-     
+            <TabPane tabId="2" >{
+              historial.length > 0 && <div> <br /> <h4 style={{ color: "#85858C", fontSize: '1.5em' }}>Historial de juegos</h4>
+                {historial.map(i => (
+                  <ListadoHistoriaDeAsignaciones data={i.documentos} />
+                ))
+                }
+              </div>
+            }
+            </TabPane>
+          </>
+        }
 
 
-          
-      
-        </TabContent>
-      
+
+
+
+
+      </TabContent>
+
 
     </Container>
   )
